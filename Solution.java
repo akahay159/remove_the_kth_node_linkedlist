a33100083df97fbeb9e16ead7d4926d91e9e87f4
Akshay Hiremath <49857679+akahay159@users.noreply.github.com>
2020-12-19 22:13:12 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,1 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode right = dummy.next; // pointing to the head of the list
+        while(n > 0){
+            right = right.next;
+            n--;
+        }
+        ListNode left = dummy;
+        while(right != null){
+            left = left.next;
+            right = right.next;
+        }
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
 
+// Time Complexity is O(n)
+// Space Complexity is O(1)
